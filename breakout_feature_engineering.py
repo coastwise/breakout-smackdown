947d6ce264a85b555d01d5a6a0f834cb70ee2066
Patrick McKenna <patrick.a.mckenna@gmail.com>
2017-08-19 08:53:04 -0400

    fix integer divide in state computation

--- breakout_feature_engineering.py
+++ breakout_feature_engineering.py
@@ -63,8 +63,8 @@ def ball_and_paddle_state( observation, delta ):
 	paddle_start, paddle_end = detect_paddle( observation )
 	ball_x, ball_y = detect_ball( delta, paddle_start, paddle_end )
 
-	paddle_f = paddle_start / ( observation.shape[ 1 ] - 16 )
-	ball_x_f = ball_x / observation.shape[ 1 ]
-	ball_y_f = ball_y / observation.shape[ 0 ]
+	paddle_f = float( paddle_start ) / ( observation.shape[ 1 ] - 16 )
+	ball_x_f = float( ball_x ) / observation.shape[ 1 ]
+	ball_y_f = float( ball_y ) / observation.shape[ 0 ]
 
 	return [ paddle_f, ball_x_f, ball_y_f ]
